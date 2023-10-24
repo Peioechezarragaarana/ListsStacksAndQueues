4ccca4fb1199a1642c6d8a7290f0e4f08a9fc030
Peio Echezarraga Arana <pechezarraga001@ikasle.ehu.eus>
2023-10-24 18:42:43 +0200

    PROGRAMA AMAITUTA

--- Common/3.StacksAndQueues/GenericQueue.cs
+++ Common/3.StacksAndQueues/GenericQueue.cs
@@ -12,31 +12,38 @@ namespace Common
 
         public string AsString()
         {
+            return myList.AsString();
             //TOD= #2: Return the list as a string. Use the method already implemented in your list
-            return null;
+           
         }
 
         public int Count()
         {
             //TODO #3: Return the number of objects
-            return 0;
+            return myList.Count();
         }
 
         public void Clear()
         {
             //TODO #4: Remove all objects stored
+            myList.Clear();
         }
 
         public void Push(T value)
         {
             //TODO #5: Add a new object to the list (at the end of it)
+            myList.Add(value);
         }
 
         public T Pop()
         {
             //TODO #6: Remove the first object of the list and return it
 
-            return default(T);
+            
+            T t;
+            t = myList.Get(0);
+            myList.Remove(0);
+            return t;
         }
     }
 }
